67887902eda4f4b6de1016be9538385cf7bad268
futoid <aliekmandal24@gmail.com>
2023-11-24 17:35:34 +0530

    select-none path

--- components/path.tsx
+++ components/path.tsx
@@ -4,8 +4,8 @@ const Path = () => {
     const router = useRouter();
     const back = "- go back"
     return (
-        <div className=" flex font-medium text-neutral-600 select-none" onClick={() => router.back()}>
-            <p>{back}</p>
+        <div className=" flex font-medium text-neutral-600 select-none">
+            <button onClick={() => router.back()}>{back}</button>
         </div>
     );
 }
